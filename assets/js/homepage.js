d37d27a11cde67edf61343ce0b1ad9748e7d0a1f
Tara Evans <tarajevans@hotmail.com>
2022-07-03 22:01:13 -0400

    error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -21,12 +21,21 @@ var getUserRepos = function(user) {
   
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
+        if (response.ok) {
+          response.json().then(function(data) {
             displayRepos(data, user);
           });
-    });
+        } else {
+          alert("Error: GitHub User Not Found");
+        }
+      });
   };
   var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
     // clear old content
 repoContainerEl.textContent = "";
 repoSearchTerm.textContent = searchTerm;
