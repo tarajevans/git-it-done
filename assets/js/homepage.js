fae8f6850b4c5f3ac6a9d2fc4f5b4ad7d872dfb9
Tara Evans <tarajevans@hotmail.com>
2022-07-04 16:55:53 -0400

    display list of issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -20,15 +20,21 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-          response.json().then(function(data) {
-            displayRepos(data, user);
-          });
-        } else {
-          alert("Error: GitHub User Not Found");
-        }
-      });
+    fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
+    });
   };
   var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
